6003f71634978e0dbd9cb1bf3b966602f95bdb71
Kirill Ignatenko <ignatenkokirills@ya.ru>
2021-09-23 23:49:11 +0300

    Feature at Sprint 5

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Проект: Место
 
 Проект о различных достопримечательностях в России <br>
-Третий проект на курсе "Веб-разработчик" Яндекс.Практикум - Спринт 4
+Третий проект на курсе "Веб-разработчик" Яндекс.Практикум - Спринт 4 - 5
 
-[Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=28212%3A269) <br>
+[Ссылка на макет в Figma (спринт 4)](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=28212%3A269) <br>
+[Ссылка на макет в Figma (спринт 5)](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1) <br>
 [Ссылка на GitHub Pages](https://nakinos.github.io/mesto/index.html)
 
 ## Технологии
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,100 @@
-let body = document.querySelector('.body');
-let name = body.querySelector('.profile__name');
-let job = body.querySelector('.profile__job');
-let popup = body.querySelector('.popup');
-let formElement = popup.querySelector('.popup__form');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
+const placesList = document.querySelector('.places__list');
+const name = document.querySelector('.profile__name');
+const job = document.querySelector('.profile__job');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const addPlaceButton = document.querySelector('.profile__add-button');
 
-let editProfileButton = body.querySelector('.profile__edit-button');
-let closeFormButton = popup.querySelector('.popup__close-button');
+const editProfilePopup = document.querySelector('.popup_type_edit-profile');
+const editProfileFormElement = editProfilePopup.querySelector('.popup__form');
+const nameInput = editProfileFormElement.querySelector('.popup__input_type_name');
+const jobInput = editProfileFormElement.querySelector('.popup__input_type_job');
+const closeEditProfileButton = editProfilePopup.querySelector('.popup__close-button');
 
-function onEditButtonClick() {
+const addPlacePopup = document.querySelector('.popup_type_add-place');
+const addPlaceFormElement = addPlacePopup.querySelector('.popup__form');
+const placeNameInput = addPlaceFormElement.querySelector('.popup__input_type_place-name');
+const urlInput = addPlaceFormElement.querySelector('.popup__input_type_image-url');
+const closeAddPlaceButton = addPlacePopup.querySelector('.popup__close-button');
+
+const imagePopup = document.querySelector('.popup_type_show-image');
+const imageElement = imagePopup.querySelector('.popup__image');
+const imageDescElement = imagePopup.querySelector('.popup__image-description');
+const closeImageButton = imagePopup.querySelector('.popup__close-button');
+
+const togglePopup = popup => popup.classList.toggle('popup_opened');
+
+const addPlace = (name, link) => {
+  const placeTemplate = document.querySelector('#place-template').content;
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+  const placeImage = placeElement.querySelector('.place__image');
+  placeImage.src = link;
+  placeImage.alt = name;
+  placeElement.querySelector('.place__title').textContent = name;
+
+  placeElement.querySelector('.place__delete-button').addEventListener('click', event =>
+    event.target.closest('.place').remove());
+
+  placeElement.querySelector('.place__like-button').addEventListener('click', event =>
+    event.target.classList.toggle('place__like-button_active'));
+
+  placeImage.addEventListener('click', event => {
+    imageElement.src = event.target.src;
+    imageElement.alt = event.target.alt;
+    imageDescElement.textContent = event.target.alt;
+    togglePopup(imagePopup);
+  });
+
+  placesList.prepend(placeElement);
+};
+
+initialCards.forEach(item => addPlace(item.name, item.link));
+
+editProfileButton.addEventListener('click', () => {
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
-
-  popup.classList.add('popup_opened');
-}
-
-function onCloseFormButtonClick() {
-  popup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
+  togglePopup(editProfilePopup)
+});
+editProfileFormElement.addEventListener('submit', (event) => {
+  event.preventDefault();
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  onCloseFormButtonClick();
-}
+  togglePopup(editProfilePopup);
+});
+closeEditProfileButton.addEventListener('click', () => togglePopup(editProfilePopup));
 
-editProfileButton.addEventListener('click', onEditButtonClick);
-closeFormButton.addEventListener('click', onCloseFormButtonClick);
-formElement.addEventListener('submit', formSubmitHandler);
+addPlaceButton.addEventListener('click', () => togglePopup(addPlacePopup));
+addPlaceFormElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addPlace(placeNameInput.value, urlInput.value);
+  togglePopup(addPlacePopup);
+});
+closeAddPlaceButton.addEventListener('click', () => togglePopup(addPlacePopup));
+
+closeImageButton.addEventListener('click', () => togglePopup(imagePopup));
